a7affb85f058c22a974e9212d44d70242aff8f69
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2023-12-21 19:27:20 +0530

    Bug fix

--- backend/models/book.js
+++ backend/models/book.js
@@ -17,17 +17,11 @@ const bookSchema = new mongoose.Schema({
 
 bookSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    if(returnedObject._id) {
       returnedObject.id = returnedObject._id.toString();
       delete returnedObject._id;  
-    }
-    if(returnedObject.__v) {
       delete returnedObject.__v;
-    }
   },
 });
-// TODO Fix this error : transform appears to be called twice, 
-// necessitating the if statements
 
 const Book = mongoose.model("Book", bookSchema);
 
